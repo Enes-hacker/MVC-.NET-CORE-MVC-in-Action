d89023c1436aa40e9ece784b0db1e72ad1812be8
Enes <stu1901261004@uni-plovdiv.bg>
2022-10-26 01:35:36 +0300

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCInAction.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCInAction.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            var movie = GetMovies(); //creating obj. called movie
+           
+            return View(movie);
+        } //THAT'S ALL FOLKS ;-) 
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { movieName = "Forest Gump", releasedYear = 1994, genre = "comedy-drama"},
+                new Movie { movieName = "The Godfather", releasedYear = 1972, genre = "criminal"  }
+            };
+        }
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCInAction.Models
+{
+    public class Movie
+    {
+        //prop + 2x Tab
+        public String movieName { get; set; }
+        public int releasedYear { get; set; }
+
+        public String genre { get; set; }
+    }
+}
